5b6643022f642cdc451dfb3ff4cefe69ea0b93b1
Felipemmdo <168558669+Felipemmdo@users.noreply.github.com>
2024-09-25 23:37:04 -0300

    Update Aplicacao_urls.py

--- html Conectapop/Aplicacao_urls.py
+++ html Conectapop/Aplicacao_urls.py
@@ -6,4 +6,6 @@ app_name = 'Aplicacao'
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('relatar_problemas/', views.relatar_problemas.as_view(), name='relatar_problemas'),
+    path('enquetes/', views.enquetes.as_view(), name='enquetes'),
+    path('atualizacoes/', views.atualizacoes.as_view(), name='atualizacoes'),
 ]
