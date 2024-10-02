2f3f23a40492819151b63511e30d86331d0c1878
Felipemmdo <168558669+Felipemmdo@users.noreply.github.com>
2024-10-02 10:18:02 -0300

    Update apps.py

--- Projeto/Aplicacao/apps.py
+++ Projeto/Aplicacao/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class AplicacaoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Aplicacao'
+    name = 'Projeto.Aplicacao'
